65729f43d92622195a357a14c5d8f46278646366
ericsemrow <ericsemrow@gmail.com>
2021-08-25 13:42:00 -0500

    Goal 1 (#1)
    * Initial commit with bot connection
    
    * Working milestone
    
    * Adding keepalive
    
    * optimize message exist checks
    
    Co-authored-by: Eric Semrow <>

---
+++ main.py
@@ -0,0 +1,42 @@
+import discord
+import os
+import keepalive
+from replit import db
+
+client = discord.Client()
+print( "Starting" )
+
+@client.event
+async def on_ready():
+    print("Online")
+
+@client.event 
+async def on_message(message):
+  #messages must begin with $$   
+  if message.author != client.user and message.content.startswith("$$"):
+    # stringify the channel id and strip off the $$
+    content = message.content[2:]
+    editable = await getExistingMessage(message)
+    if editable is not None:
+      await editable.edit(content=content)
+    else:
+      newMsg = await message.channel.send(content)
+      db[str(message.channel.id)] = newMsg.id
+
+    # the user's message has to be cleaned up
+    await message.delete()
+
+
+async def getExistingMessage(message):
+  channelId = str(message.channel.id)
+  if channelId in db:
+    existingMessageId = db[channelId]
+    try:
+      return await message.channel.fetch_message(existingMessageId)
+    except discord.errors.NotFound:
+      pass
+  return None
+
+keepalive.keep_alive()
+token = os.environ.get("DISCORD_TOKEN") 
+client.run(token)
