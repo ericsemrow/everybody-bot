26188cf2048680c1aee6e71cba0f079d8892feb9
Eric Semrow <ericsemrow@gmail.com>
2021-08-25 19:40:35 +0000

    Added potential log channel "everybody-bot-log"

--- main.py
+++ main.py
@@ -23,6 +23,12 @@ async def on_message(message):
       newMsg = await message.channel.send(content)
       db[str(message.channel.id)] = newMsg.id
 
+    # if there's an everybody-bot-log channel then
+    # plop the message in there for posterity
+    logChannel = getLogChannel(message)
+    if logChannel is not None:
+      await logChannel.send(f"```{content}``` by {message.author} in <#{message.channel.id}>")
+
     # the user's message has to be cleaned up
     await message.delete()
 
@@ -36,6 +42,9 @@ async def getExistingMessage(message):
     except discord.errors.NotFound:
       pass
   return None
+
+def getLogChannel(message):
+  return discord.utils.get(message.guild.channels, name="everybody-bot-log")
 
 keepalive.keep_alive()
 token = os.environ.get("DISCORD_TOKEN") 
